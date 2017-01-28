5a02b9016d587454c5498fd04d666db929d37687
ALEXANDER-DESKT\Alex <at.webdes@gmail.com>
2017-01-27 20:11:38 -0500

    Certificate added more props

---
+++ src/AlexanderTsema.Storage.Concretes/Migrations/20170128011111_Certificate_props.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AlexanderTsema.Storage.Concretes.Migrations
+{
+    public partial class Certificate_props : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Authority",
+                table: "Certificate",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Certificate",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RecieveDate",
+                table: "Certificate",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Authority",
+                table: "Certificate");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Certificate");
+
+            migrationBuilder.DropColumn(
+                name: "RecieveDate",
+                table: "Certificate");
+        }
+    }
+}
--- src/AlexanderTsema.Storage.Entities/Entities/Certificate.cs
+++ src/AlexanderTsema.Storage.Entities/Entities/Certificate.cs
@@ -4,6 +4,9 @@ namespace AlexanderTsema.Storage.Entities.Entities
 {
     public class Certificate : BaseEntity
     {
+        public String Name { get; set; }
+        public String Authority { get; set; }
+        public DateTime RecieveDate { get; set; }
         public String Image { get; set; }
     }
 }
