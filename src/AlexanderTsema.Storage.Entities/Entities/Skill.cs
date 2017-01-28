222411ca24ac6622b32cd78ec268e78f260c1dec
ALEXANDER-DESKT\Alex <at.webdes@gmail.com>
2017-01-27 21:10:25 -0500

    PortfolioItemCategory controller added

--- src/AlexanderTsema.Storage.Entities/Entities/Skill.cs
+++ src/AlexanderTsema.Storage.Entities/Entities/Skill.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AlexanderTsema.Storage.Entities.Entities
 {
     public class Skill : BaseEntity
     {
-        public String Name { get; set; } 
+        public String Name { get; set; }
         public Int16 Priority { get; set; }
         public SkillCategory SkillCategory { get; set; }
     }
---
+++ src/AlexanderTsema.WebServices/Controllers/PortfolioItemCategoryController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AlexanderTsema.Storage.Entities.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AlexanderTsema.WebServices.Controllers
+{
+    [Route("api/[controller]")]
+    public class PortfolioItemCategoryController : Controller
+    {
+        private readonly AlexanderTsema.Storage.Abstractions.Core.IStorage _storage;
+        public PortfolioItemCategoryController(AlexanderTsema.Storage.Abstractions.Core.IStorage storage)
+        {
+            this._storage = storage;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<AlexanderTsema.Storage.Entities.Entities.PortfolioItemCategory>> Get()
+        {
+            return await this._storage.GetRepository<AlexanderTsema.Storage.Abstractions.Repositories.IPortfolioItemCategoryRepository>().AllAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<PortfolioItemCategory> Get(int id)
+        {
+            return await this._storage.GetRepository<AlexanderTsema.Storage.Abstractions.Repositories.IPortfolioItemCategoryRepository>().SingleAsync(id);
+        }
+
+        //        [Authorize]
+        [HttpPost]
+        public async Task Post([FromBody]AlexanderTsema.Storage.Entities.Entities.PortfolioItemCategory portfolioItemCategory)
+        {
+            await this._storage.GetRepository<AlexanderTsema.Storage.Abstractions.Repositories.IPortfolioItemCategoryRepository>().CreateAsync(portfolioItemCategory);
+        }
+
+        //        [Authorize]
+        [HttpPut("{id}")]
+        public async Task Put([FromBody]AlexanderTsema.Storage.Entities.Entities.PortfolioItemCategory portfolioItemCategory)
+        {
+            await this._storage.GetRepository<AlexanderTsema.Storage.Abstractions.Repositories.IPortfolioItemCategoryRepository>().UpdateAsync(portfolioItemCategory);
+        }
+
+        //        [Authorize]
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            await this._storage.GetRepository<AlexanderTsema.Storage.Abstractions.Repositories.IPortfolioItemCategoryRepository>().DeleteAsync(id);
+        }
+    }
+}
