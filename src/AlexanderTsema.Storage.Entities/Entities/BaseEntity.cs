0fec4faff323c092db870e16e96e69a937fd2fdb
ALEXANDER-DESKT\Alex <at.webdes@gmail.com>
2017-02-10 22:39:02 -0500

    Timestamp modified

--- src/AlexanderTsema.Storage.Entities/Entities/BaseEntity.cs
+++ src/AlexanderTsema.Storage.Entities/Entities/BaseEntity.cs
@@ -4,15 +4,8 @@ namespace AlexanderTsema.Storage.Entities.Entities
 {
     public abstract class BaseEntity
     {
-        private DateTime _timestamp;
-
         public Int16 Id { get; set; }
 
-        public DateTime Timestamp
-        {
-            get { return _timestamp; }
-            set { _timestamp = DateTime.UtcNow; }
-        }
-
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
     }
 }
